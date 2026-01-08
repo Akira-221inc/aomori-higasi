cf34cc1d233f2d82f1f73f800ee22907e9ade71b
Akira-221inc <b1023077@fun.ac.jp>
2026-01-08 12:14:33 +0900

    ほぼ完成６

--- aomori-higasi/Assets/HONBAN/9-MiniGame06/Button3D.cs
+++ aomori-higasi/Assets/HONBAN/9-MiniGame06/Button3D.cs
@@ -10,46 +10,4 @@ public class Button3D : MonoBehaviour
     }
 
     public ButtonType type;
-    public MiniGame06Manager manager;
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            int mask = LayerMask.GetMask("Button");
-
-            if (Physics.Raycast(ray, out hit, 100f, mask))
-            {
-                Debug.Log("Button命中: " + hit.collider.name);
-                hit.collider.GetComponent<Button3D>()?.OnPressed();
-            }
-        }
-    }
-
-    public void OnPressed()
-    {
-        if (manager == null)
-        {
-            Debug.LogError("Manager未設定");
-            return;
-        }
-
-        Debug.Log("押された: " + type);
-
-        switch (type)
-        {
-            case ButtonType.Left:
-                manager.Prev();
-                break;
-            case ButtonType.Right:
-                manager.Next();
-                break;
-            case ButtonType.Decide:
-                manager.Decide();
-                break;
-        }
-    }
 }
--- aomori-higasi/Assets/HONBAN/9-MiniGame06/MiniGame06Manager.cs
+++ aomori-higasi/Assets/HONBAN/9-MiniGame06/MiniGame06Manager.cs
@@ -12,6 +12,44 @@ public class MiniGame06Manager : MonoBehaviour
         UpdateView();
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            int mask = LayerMask.GetMask("Button");
+
+            if (Physics.Raycast(ray, out hit, 100f, mask))
+            {
+                var button = hit.collider.GetComponent<Button3D>();
+                if (button != null)
+                {
+                    OnButtonPressed(button.type);
+                }
+            }
+        }
+    }
+
+    void OnButtonPressed(Button3D.ButtonType type)
+    {
+        Debug.Log("押された: " + type);
+
+        switch (type)
+        {
+            case Button3D.ButtonType.Left:
+                Prev();
+                break;
+            case Button3D.ButtonType.Right:
+                Next();
+                break;
+            case Button3D.ButtonType.Decide:
+                Decide();
+                break;
+        }
+    }
+
     void UpdateView()
     {
         foreach (var m in medicines)
@@ -28,15 +66,14 @@ public class MiniGame06Manager : MonoBehaviour
 
     public void Prev()
     {
-        index--;
-        if (index < 0)
-            index = medicines.Length - 1;
-
+        index = (index - 1 + medicines.Length) % medicines.Length;
         UpdateView();
     }
 
     public void Decide()
     {
+        Debug.Log("現在: " + medicines[index].medicineId);
+
         if (medicines[index].medicineId == correctId)
             Debug.Log("成功！");
         else
