425f66fcdcdeb261ab7415d1fda51a3f35072b3c
Akira-221inc <b1023077@fun.ac.jp>
2026-01-13 12:46:43 +0900

    調整４

--- aomori-higasi/Assets/HONBAN/2-Movie01/MovieEndController.cs
+++ aomori-higasi/Assets/HONBAN/2-Movie01/MovieEndController.cs
@@ -1,24 +1,45 @@
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MovieEndController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public string nextSceneName;
+    public Camera targetCamera;
+    public float blackTime = 5f;
 
     private float lastClickTime = 0f;
     private float doubleClickThreshold = 0.3f;
 
     void Start()
     {
-        // 動画が終わったら呼ばれる
+        // Cameraを黒に
+        targetCamera.clearFlags = CameraClearFlags.SolidColor;
+        targetCamera.backgroundColor = Color.black;
+
+        // 動画を最初は「完全透明」にする
+        videoPlayer.targetCameraAlpha = 0f;
+        videoPlayer.Stop();
+
         videoPlayer.loopPointReached += OnMovieEnd;
+
+        StartCoroutine(PlayMovieAfterBlack());
+    }
+
+    IEnumerator PlayMovieAfterBlack()
+    {
+        // 5秒暗転
+        yield return new WaitForSeconds(blackTime);
+
+        // 動画を表示
+        videoPlayer.targetCameraAlpha = 1f;
+        videoPlayer.Play();
     }
 
     void Update()
     {
-        // マウス左クリック
         if (Input.GetMouseButtonDown(0))
         {
             float timeSinceLastClick = Time.time - lastClickTime;
